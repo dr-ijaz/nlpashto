7aa9c7bcd869d142ce84a9b28bd623620bf9a819
ijazul Haq <ijazul.haq@outlook.com>
2024-02-25 13:22:22 +0800

    init

---
+++ nlpashto/__init__.py
@@ -0,0 +1,6 @@
+from .tokenization import Tokenizer
+from .word_segmentation import Segmenter
+from .pos_tagging import POSTagger
+from .sentiment_analysis import POLD
+from .utils import Cleaner
+from . import pashto
